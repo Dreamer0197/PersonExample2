38f4e5639198780b9964f0a395499a13dd13c96d
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-17 21:06:12 +0300

    Finished

--- PersonExample/Person.h
+++ PersonExample/Person.h
@@ -4,9 +4,46 @@
 //
 //  Created by Kaan Şengün on 17.11.2023.
 //
+#include <iostream>
+using namespace std;
 
-#ifndef Person_h
-#define Person_h
+class Person{
+private:
+    string perName;
+public:
+    Person(string name){perName=name;};
+    string getName()const{return perName;}
+    virtual string getNation()const{return " ";}
+    virtual void sayHi(const Person &person){
+        cout<<perName<<" says Hi "<<person.perName<<endl;
+    }
+    
+};
 
+class DeutchePerson:public Person{
+public:
+    DeutchePerson(string name):Person(name){}
+    string getNation()const{return "Deutch";}
+    void sayHi(const Person &person){
+        if(getNation()==person.getNation()){
+            cout<<getName()<<" says Hallo "<<person.getName()<<endl;
+        }
+        else{
+            Person::sayHi(person);
+        }
+    }
+};
+class TurkishPerson:public Person{
+public:
+    TurkishPerson(string name):Person(name){}
+    string getNation()const{return "Turkish";}
+    void sayHi(const Person &person){
+        if(getNation()==person.getNation()){
+            cout<<getName()<<" says Merhaba "<<person.getName()<<endl;
+        }
+        else{
+            Person::sayHi(person);
+        }
+    }
+};
 
-#endif /* Person_h */
--- PersonExample/main.cpp
+++ PersonExample/main.cpp
@@ -6,9 +6,26 @@
 //
 
 #include <iostream>
+#include "Person.h"
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Person *pArr[]={new TurkishPerson("Ogun"),new TurkishPerson("Duygu"),new TurkishPerson("Oguz"),new DeutchePerson("Thomas"),new DeutchePerson("Carl")};
+    
+    int numberOfPerson=sizeof(pArr)/sizeof(pArr[0]);
+    for(int i=0;i<numberOfPerson;i++){
+        for(int j=0;j<numberOfPerson;j++){
+            if(i!=j){
+                pArr[i]->sayHi(*pArr[j]);
+            }
+        }
+        cout<<"\n";
+    }
+    
+    
+    
+    
+    
     return 0;
 }
